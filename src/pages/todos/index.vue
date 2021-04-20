152cde120ac8f3a0c4e55beba1ec8982124dfd65
Paul Bastowski <pbastowski@gmail.com>
2021-04-20 17:47:30 +0200

    add todos route (in pages folder) create a Svelte mini-todos component create a vue wrapper for the Svelte component - the pages plugin currently only auto mounts vue components

---
+++ src/pages/todos/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <my-todos :todos="todos" @update="console.log('EV:', $event)" />
+    <hr />
+    <pre>{{ todos }}</pre>
+</template>
+
+<script setup>
+    import './todos.wc.svelte'
+    const todos = ['one', 'two']
+</script>
---
+++ src/pages/todos/todos.wc.svelte
@@ -0,0 +1,27 @@
+<svelte:options tag="my-todos" />
+
+<script>
+    export let todos = []
+    import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	let me
+</script>
+
+<slot name="head">
+    <h1>TODOS</h1>
+</slot>
+
+<slot>
+    {#each todos as item}
+        <li on:click={e=>{
+            todos=todos.filter(i=>i!==item)
+            dispatch('update', todos)
+        }} >{item}</li>
+    {/each}
+    <pre>{todos}</pre>
+</slot>
+
+<hr>
+<pre>{JSON.stringify($$props)}</pre>
+<hr>
