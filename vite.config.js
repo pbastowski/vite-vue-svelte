39f45e8bfab18f01418c8f66d27dcdb66685d2c8
Paul Bastowski <pbastowski@gmail.com>
2021-04-20 17:41:34 +0200

    add svelte config only minify when building for production

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,44 @@ import WindiCSS from 'vite-plugin-windicss'
 import ViteComponents from 'vite-plugin-components'
 import Markdown from 'vite-plugin-md'
 import Pages from 'vite-plugin-pages'
+import svelte from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        Vue({
-            include: [/\.vue$/, /\.md$/], // <--
-        }),
-        Pages({
-            exclude: ['**/components/*.vue'],
-            extensions: ['vue', 'js', 'md'],
-        }),
-        Markdown(),
-        WindiCSS({
-            preflight: false,
-        }),
-        ViteComponents({
-            // allow auto load markdown components under `./src/components/`
-            extensions: ['vue', 'md'],
+module.exports = defineConfig(({ command, mode }) => {
+    return {
+        plugins: [
+            Vue({
+                include: [/\.vue$/, /\.md$/], // <--
+            }),
+            Pages({
+                exclude: ['**/components/*.vue'],
+                extensions: ['vue', 'js', 'md'],
+            }),
+            Markdown(),
+            WindiCSS({
+                preflight: false,
+            }),
+            ViteComponents({
+                // allow auto load markdown components under `./src/components/`
+                extensions: ['vue', 'md', 'svelte'],
 
-            // allow auto import and register components used in markdown
-            customLoaderMatcher: path => path.endsWith('.md'),
-        }),
-    ],
+                // allow auto import and register components used in markdown
+                customLoaderMatcher: path => path.endsWith('.md'),
+            }),
+
+            // Allow normal svelte components as well a svelte custome elements
+            svelte({
+                compilerOptions: { customElement: true },
+                include: /\.wc\.svelte$/,
+            }),
+            svelte({
+                compilerOptions: { customElement: false },
+                include: /\\.svelte$/,
+                exclude: /\.wc\.svelte$/,
+            }),
+        ],
+        build: {
+            minify: mode === 'production',
+        },
+    }
 })
