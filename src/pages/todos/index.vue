38d76c185e1a2ef41a1c0632b7722f0c74ce8f2b
Paul Bastowski <pbastowski@gmail.com>
2021-05-02 23:24:07 +0200

    change to Vue2 format, because Vue3 format does not work (or I don't know how to use it properly) provide updateTodos callback to todos WC, because events dispatched from svelte don't bubble through to Vue

--- src/pages/todos/index.vue
+++ src/pages/todos/index.vue
@@ -1,16 +1,33 @@
 <template>
-    <my-todos :todos="todos" @update="console.log('EV:', $event)" />
+    <my-todos
+        :todos="todos"
+        @update="console.log('EV:', $event)"
+        :updateTodos="updateTodos"
+    />
+
     <hr />
     <pre>VUE: {{ todos }}</pre>
 </template>
 
-<script setup>
-    import './todos.wc.svelte'
-    const todos = [
-        { title: 'Item 1' },
-        { title: 'Another Item' },
-        { title: 'Robot 3' },
-    ]
-
+<script>
     console.log('! todos/index')
+
+    import './todos.wc.svelte'
+
+    export default {
+        data: () => ({
+            todos: [
+                { title: 'Item 1' },
+                { title: 'Another Item' },
+                { title: 'Robot 3' },
+            ],
+        }),
+
+        methods: {
+            updateTodos(newTodos) {
+                this.todos = newTodos
+                console.log('VUE: update:', this.todos, newTodos)
+            },
+        },
+    }
 </script>
