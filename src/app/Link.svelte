9e69528832a9b1b75f909e44693edfc19c39ae73
Paul Bastowski <pbastowski@gmail.com>
2021-05-06 23:11:50 +0200

    remove unused files

--- src/app/Link.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { router } from './store.js'
-
-    export let to
-
-    console.log('LINK: router:', $router)
-</script>
-
-<a href="javascript:" on:click={() => $router.push(to)}><slot /></a>
